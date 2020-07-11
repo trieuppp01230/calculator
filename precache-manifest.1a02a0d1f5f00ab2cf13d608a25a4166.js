16d108a5d2f56b11e32f3955b36bb1f66d05b672
dinhtrieu <trieunguyencd17tt10@gmail.com>
2020-07-11 12:59:39 +0700

    Updates

---
+++ precache-manifest.1a02a0d1f5f00ab2cf13d608a25a4166.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674899146019a90efd08703ddd30baa6",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e460ba74f745abdc5c91",
+    "url": "/calculator/static/css/main.6d0c4807.chunk.css"
+  },
+  {
+    "revision": "cbf7108523a79b100a8d",
+    "url": "/calculator/static/js/2.ecca2fe9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.ecca2fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e460ba74f745abdc5c91",
+    "url": "/calculator/static/js/main.235efa15.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
